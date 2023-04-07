fafa43ce3280150dea8596519d4406887dcdf1b5
Israel Guillermo <israelguillermo22@gmail.com>
2023-04-07 02:20:56 -0500

    updates to make responsive

--- israelguillermo.io/src/components/AppNavBar.tsx
+++ israelguillermo.io/src/components/AppNavBar.tsx
@@ -10,118 +10,29 @@ import Typography from '@material-ui/core/Typography';
 import Badge from '@material-ui/core/Badge';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
-import { useState } from 'react';
-import { Box } from '@material-ui/core';
+import { useEffect, useState } from 'react';
+import { Box, useMediaQuery, useTheme } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import { MobileMenu } from './MobileMenu';
-
+import CloseIcon from '@material-ui/icons/Close';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     grow: {
       flexGrow: 1
-    },
-    menuButton: {
-      marginRight: theme.spacing(2)
-    },
-    title: {
-      display: 'none',
-      [theme.breakpoints.up('sm')]: {
-        display: 'block'
-      }
-    },
-    search: {
-      position: 'relative',
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: alpha(theme.palette.common.white, 0.15),
-      '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25)
-      },
-      marginRight: theme.spacing(2),
-      marginLeft: 0,
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(3),
-        width: 'auto'
-      }
-    },
-    searchIcon: {
-      padding: theme.spacing(0, 2),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
-    inputRoot: {
-      color: 'inherit'
-    },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('md')]: {
-        width: '20ch'
-      }
-    },
-    sectionDesktop: {
-      display: 'none',
-      [theme.breakpoints.up('sm')]: {
-        display: 'flex'
-      }
-    },
-    sectionMobile: {
-      display: 'flex',
-      [theme.breakpoints.up('sm')]: {
-        display: 'none'
-      }
-    },
-    popOverClasses: {
-      width: '100%',
-      height: '100%'
     }
   })
 );
 
 export default function AppNavBar() {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
-    useState<null | HTMLElement>(null);
 
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
+  const handleMobileMenuOpen = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
   };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-
-  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
-
-  const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Resume</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  );
+  const theme = useTheme();
+  const smallScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
     <div className={classes.grow}>
@@ -129,57 +40,39 @@ export default function AppNavBar() {
         position='sticky'
         style={{
           backgroundColor: 'transparent',
-          color: 'red',
+          color: 'black',
           width: '100vw',
           border: 'none'
         }}
       >
         <Toolbar>
-          <IconButton
-            edge='start'
-            className={classes.menuButton}
-            color='inherit'
-            aria-label='open drawer'
-          >
-            Home
-          </IconButton>
+          <IconButton>Home</IconButton>
           <Typography variant='h6' noWrap>
             Israel Guillermo
           </Typography>
 
-          <div className={classes.grow} />
-          <div className={classes.sectionDesktop}>
-            <IconButton aria-label='show 4 new mails' color='inherit'>
-              Resume
-            </IconButton>
-            <IconButton aria-label='show 17 new notifications' color='inherit'>
-              Projects
-            </IconButton>
-            <IconButton
-              edge='end'
-              aria-label='account of current user'
-              aria-controls={menuId}
-              aria-haspopup='true'
-              // onClick={handleProfileMenuOpen}
-              color='inherit'
-            >
-              Contact
-            </IconButton>
-          </div>
-          <div className={classes.sectionMobile}>
-            <IconButton
-              aria-label='show more'
-              aria-haspopup='true'
-              onClick={handleMobileMenuOpen}
-              color='inherit'
-            >
-              <MenuIcon />
-            </IconButton>
-          </div>
+          {smallScreen ? (
+            <>
+              <div className={classes.grow} />
+              <div>
+                <IconButton onClick={handleMobileMenuOpen}>
+                  {isMobileMenuOpen ? <CloseIcon /> : <MenuIcon />}
+                </IconButton>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className={classes.grow} />
+              <div>
+                <IconButton>Resume</IconButton>
+                <IconButton>Projects</IconButton>
+                <IconButton>Contact</IconButton>
+              </div>
+            </>
+          )}
         </Toolbar>
       </Box>
-      <MobileMenu open={true} />
-      {renderMenu}
+      <MobileMenu open={isMobileMenuOpen} />
     </div>
   );
 }
