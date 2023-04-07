0321d5022c66321973cabe13190bf8b9a3776674
Israel Guillermo <israelguillermo22@gmail.com>
2023-04-07 02:20:00 -0500

    adds homepage to index

--- israelguillermo.io/src/pages/index.tsx
+++ israelguillermo.io/src/pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
 import AppNavBar from '@/components/AppNavBar';
+import { HomePage } from './homepage';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,6 +18,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <AppNavBar />
+        <HomePage />
       </main>
     </>
   );
