c47da5febef1446ed467617755a3dc9878651476
Israel Guillermo <israelguillermo22@gmail.com>
2023-04-07 02:20:20 -0500

    creates mobile menu

--- israelguillermo.io/src/components/MobileMenu.tsx
+++ israelguillermo.io/src/components/MobileMenu.tsx
@@ -1,39 +1,50 @@
-import { Box, IconButton, Menu, MenuItem } from '@material-ui/core';
+import {
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  useMediaQuery,
+  useTheme
+} from '@material-ui/core';
+import zIndex from '@material-ui/core/styles/zIndex';
 
 interface Props {
   open: boolean;
 }
 
 export function MobileMenu({ open }: Props) {
-  return (
-    open && (
-      <Box
-        style={{
-          height: '100vh',
-          width: '100vw',
-          display: 'flex',
-          alignItems: 'center',
-          flexDirection: 'column'
-        }}
-      >
-        <>
-          <MenuItem>
-            <IconButton aria-label='show 4 new mails' color='inherit'>
-              Resume
-            </IconButton>
-          </MenuItem>
-          <MenuItem>
-            <IconButton aria-label='show 4 new mails' color='inherit'>
-              Projects
-            </IconButton>
-          </MenuItem>
-          <MenuItem>
-            <IconButton aria-label='show 4 new mails' color='inherit'>
-              Contact
-            </IconButton>
-          </MenuItem>
-        </>
-      </Box>
-    )
-  );
+  const theme = useTheme();
+  const smallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  return open && smallScreen ? (
+    <Box
+      style={{
+        height: '100vh',
+        width: '100vw',
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        position: 'absolute',
+        backgroundColor: '#f3f3f3',
+        padding: theme.spacing(1)
+      }}
+    >
+      <>
+        <MenuItem>
+          <IconButton aria-label='show 4 new mails' color='inherit'>
+            Resume
+          </IconButton>
+        </MenuItem>
+        <MenuItem>
+          <IconButton aria-label='show 4 new mails' color='inherit'>
+            Projects
+          </IconButton>
+        </MenuItem>
+        <MenuItem>
+          <IconButton aria-label='show 4 new mails' color='inherit'>
+            Contact
+          </IconButton>
+        </MenuItem>
+      </>
+    </Box>
+  ) : null;
 }
